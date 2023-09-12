325de661a2237f42cecf6a593b38412ee9dc33ac
VishnuVS <vishnusenthil222@gmail.com>
2023-09-12 20:27:09 +0530

    render list using ScrollView

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView,FlatList } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [saveText, setSaveText] = useState('test');
+  const [goal, setGoal] = useState([])
+
+  function SaveChangeText(e) {
+    setSaveText(e)
+  }
+
+  function CreateTask() {
+    saveText.length ?
+      setGoal(prev => [...prev, saveText]) : "";
+    // setSaveText('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputField} value={saveText} placeholder="new task" onChangeText={SaveChangeText} />
+        <View style={{ flex: 1, padding: 5 }}>
+          <Button title='Add' color="#536976" onPress={CreateTask} />
+        </View>
+      </View>
+      <Button title='reset' color="#536976" onPress={() => setGoal([])} />
+      <Text style={{ fontSize: 20, margin: 30,marginBottom:20 }}>Tasks</Text>
+
+
+      {goal.length==0 ? <Text style={styles.noTask}>No Tasks to do..</Text> : ""}
+
+      { goal.length> 0 ?
+      <View style={styles.goalsContainer}>
+      <ScrollView >
+        {
+          goal.map((e, i) => (
+            <View style={styles.listItem} key={i}>
+              <Text style={{ color: 'white' }}>{e}</Text>
+            </View>
+          )
+          )
+        }
+      </ScrollView>
+      </View>
+      : "" }
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#292e49',
+    paddingTop: 40,
+    padding: 10,
+    paddingBottom: 20,
+    marginBottom: 20
   },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 45,
+    color: 'white'
+  },
+  inputField: {
+    height: 42,
+    width: '75%',
+    borderWidth: 1,
+    paddingLeft: 8,
+    borderRadius: 8,
+    backgroundColor: '#292e49',
+    color: 'white',
+    fontSize: 25
+  },
+  noTask:{
+    textAlign:'center',
+    color:'darkgray'
+  },
+  listItem: {
+    width: '100%',
+    height: 80,
+    backgroundColor: '#292e49',
+    padding: 8,
+    marginBottom: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 22,
+  },
+  goalsContainer: {
+    height: '80%',
+    paddingBottom:20
+    
+  }
 });
